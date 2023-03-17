39b9e031d836836618af0ebef33b520963c15a2e
Johnydavid <dropin2johny@gmail.com>
2023-03-17 17:20:30 +0530

    c

--- server/db.js
+++ server/db.js
@@ -1,17 +1,23 @@
-const mongoose = require('mongoose');
+const { Db } = require("mongodb");
+const mongoose = require("mongoose");
 
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, {});
+// const db = mongoose.connection;
+// db.once("open", () => {
+//   console.log("Connection Successful");
+// });
+// db.on('error', ()=>{
+//     console.log('Connection Failed');
+// });
 
-module.exports = () =>{
-    try{
-        mongoose.connect(process.env.ATLAS_URI, {});
-        console.log("Connection success")
+module.exports = () => {
+  try {
+    mongoose.connect(process.env.ATLAS_URI, {});
+    console.log("Connected to Database successfully");
+  } catch (error) {
+    console.log(error);
 
-    }
-    catch(error){
-
-        console.log(error);
-
-        console.log("connection failed");
-
-    }
-}
+    console.log("connection failed");
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connection = require('./db')
 
-//  Middleware
+// Database Connection
+
+connection();
+
+//  Middleware to create web server
 
 app.use(express.json());
 app.use(cors());
 
 
-const port = process.env.PORT || 8080;
+const port = Number(process.env.PORT) || 8080;
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
